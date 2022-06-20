23d6b275235098ce19171a285b62e09435cbd713
sukeroV <he429855@gmail.com>
2022-06-21 03:35:19 +0800

    12345678910

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
-
+// kong
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
